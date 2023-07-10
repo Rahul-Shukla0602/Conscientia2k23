2a2cb5e6dc13108fa472e890fb9fb1cea47c7417
Captainofcode <shuklarahul0206@gmail.com>
2023-07-10 16:21:08 +0530

    origin change

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://conscientia2k23.vercel.app",
+		origin:"https://www.conscientia.co.in",
 		credentials:true,
 	})
 )
