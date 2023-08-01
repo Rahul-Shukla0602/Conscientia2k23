5d6152cdb8cd37d0995cfce9170dc10b47af6b5a
Captainofcode <shuklarahul0206@gmail.com>
2023-08-01 15:52:37 +0530

    api changes

--- server/index.js
+++ server/index.js
@@ -24,8 +24,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		// origin:"https://www.conscientia.co.in",
-		origin:"http://localhost:3000",
+		origin:"https://www.conscientia.co.in",
+		// origin:"http://localhost:3000",
 		credentials:true,
 	})
 )
