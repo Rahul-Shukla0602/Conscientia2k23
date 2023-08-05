7211b2ab51d0282973b4e83455be84fdc5b3e3e4
Captainofcode <shuklarahul0206@gmail.com>
2023-08-05 13:04:30 +0530

    merch prices change

--- src/data/merchs.js
+++ src/data/merchs.js
@@ -5,7 +5,7 @@ export const Merchs = [
         title: "Black T-Shirt",
         img:'https://res.cloudinary.com/do07okngw/image/upload/v1691211709/black_shirt_lstwzo.jpg',
         desc: "This black t-shirt boasts a mesmerizing Cosmic Carnival theme, capturing the essence of a celestial circus. Vividly colored cosmic creatures dance amid sparkling stars and swirling galaxies, creating an enchanting spectacle. Embrace the mystique of the universe with this captivating wearable art piece.",
-        rate: 699,
+        rate: 450,
         size: ['S','M','L','XL'],
         color: 'Black'
     },
@@ -15,7 +15,7 @@ export const Merchs = [
         title: "White T-Shirt",
         img:'https://res.cloudinary.com/do07okngw/image/upload/v1691211696/White_thsirt-1_v1olxb.jpg',
         desc: "Celebrate space exploration with this white t-shirt inspired by INSPIRESAT-1, the remarkable satellite by IIST. The shirt showcases the satellite's sleek design orbiting Earth amid constellations. A symbol of scientific achievement, it embodies innovation and curiosity. Wear this tee to express your passion for space and technological advancements.",
-        rate: 699,
+        rate: 450,
         size: ['S','M','L','XL'],
         color: 'White'
     },
@@ -25,7 +25,7 @@ export const Merchs = [
         title: "Anime T-Shirt",
         img:'https://res.cloudinary.com/do07okngw/image/upload/v1691211716/anime_l5s5gj.jpg',
         desc: "Step into the vibrant world of anime with this dark blue t-shirt featuring the iconic One Punch Man character, Saitama. With his deadpan expression and unmistakable yellow cape, Saitama stands ready for action. Embrace your inner hero and showcase your love for anime with this eye-catching and stylish tee.",
-        rate: 799,
+        rate: 450,
         size: ['S','M','L','XL'],
         color: 'Mix'
     },
@@ -35,7 +35,7 @@ export const Merchs = [
         title: "Polo T-Shirt",
         img:'https://res.cloudinary.com/do07okngw/image/upload/v1691211536/polo_u2twl1.png',
         desc: "Experience a touch of elegance and professionalism with the IIST Official Polo T-shirt. Crafted with premium materials, this classic polo exudes sophistication while proudly displaying the IIST logo, symbolizing excellence in space research and education. Stay comfortable and distinguished, making a statement of your association with India's premier space institute.",
-        rate: 999,
+        rate: 600,
         size: ['S','M','L','XL'],
         color: 'Blue'
     },
@@ -45,7 +45,7 @@ export const Merchs = [
         title: "Sweatshirt",
         img:'https://res.cloudinary.com/do07okngw/image/upload/v1691211721/sweatshirt_sxmrza.jpg',
         desc: "Embrace warmth and style with the IIST Official Sweatshirt. Designed for comfort and durability, this cozy sweatshirt proudly bears the distinguished IIST logo, representing the forefront of space research and education. Whether on campus or in chilly evenings, exude a sense of pride while staying snug in this premium-quality garment.",
-        rate: 999,
+        rate: 800,
         size: ['S','M','L','XL'],
         color: 'Black'
     },
@@ -55,7 +55,7 @@ export const Merchs = [
         title: "poloWhite",
         img:'https://res.cloudinary.com/do07okngw/image/upload/v1691211399/polo-white_apb6gk.png',
         desc: "Embrace warmth and style with the IIST Official Sweatshirt. Designed for comfort and durability, this cozy sweatshirt proudly bears the distinguished IIST logo, representing the forefront of space research and education. Whether on campus or in chilly evenings, exude a sense of pride while staying snug in this premium-quality garment.",
-        rate: 999,
+        rate: 600,
         size: ['S','M','L','XL'],
         color: 'White'
     },
